4595c5d5934fabfad2f937ba5d848a1797482a8d
Łukasz Walaszczyk <l.walaszczyk@interia.pl>
2025-04-02 18:58:15 +0200

    initialize Vue 3 project with Vite setup and basic configuration

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+
+  </header>
+
+  <main>
+
+  </main>
+</template>
+
+<style scoped>
+header {
+  /*  */
+}
+
+@media (min-width: 1024px) {
+  header {
+    /*  */
+  }
+}
+</style>
